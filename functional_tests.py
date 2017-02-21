c240dfd67cde82fc064c9a65dabeaef01da367ca
hadesshark <hades_shark@hotmail.com>
2017-02-21 22:48:39 +0800

    functional_tests.py 修改成 unittest 方法

--- functional_tests.py
+++ functional_tests.py
@@ -1,33 +1,45 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
 
-# Edith 聽到一個很酷的新線上待辦事項 app。
-# 她去查看它的首頁
-browser.get('http://localhost:8000')
+    def setUp(self):
+        self.browser = webdriver.Firefox()
 
-# 她發現網頁標題與標頭顯示待辦事項清單
-assert 'To-Do' in browser.title
+    def tearDown(self):
+        self.browser.quit()
 
-# 她馬上受邀輸入一個待辦事項
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith 聽到一個很酷的新線上待辦事項 app。
+        # 她去查看它的首頁
+        self.browser.get('http://localhost:8000')
 
-# 她在文字方塊輸入“購買孔雀羽毛”
-#（Edith 的興趣是綁蒼蠅魚餌）
+        # 她發現網頁標題與標頭顯示待辦事項清單
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
 
-# 當她按下 enter 時，網頁會更新，現在網頁列出
-# "1: 購買孔雀羽毛"，一個待辦事項清單項目
+        # 她馬上受邀輸入一個待辦事項
 
-# 此時仍然有一個文字方塊，讓她可以加入另一個項目。
-# 她輸入“使用孔雀羽毛來製作一隻蒼蠅”（Edith 非常有條理）
+        # 她在文字方塊輸入“購買孔雀羽毛”
+        #（Edith 的興趣是綁蒼蠅魚餌）
 
-# 網頁再次更新，現在她的清單有這兩個項目
+        # 當她按下 enter 時，網頁會更新，現在網頁列出
+        # "1: 購買孔雀羽毛"，一個待辦事項清單項目
 
-# Edith 不知道網站能否記得她的清單
-# 接著她看到網站產生一個唯一的 URL 給她
-# 網頁有一些文字說明這個效果
+        # 此時仍然有一個文字方塊，讓她可以加入另一個項目。
+        # 她輸入“使用孔雀羽毛來製作一隻蒼蠅”（Edith 非常有條理）
 
-# 她前往那個 URL - 她的待辦清單仍然在那裡。
+        # 網頁再次更新，現在她的清單有這兩個項目
 
-# 她很滿意地上床睡覺
+        # Edith 不知道網站能否記得她的清單
+        # 接著她看到網站產生一個唯一的 URL 給她
+        # 網頁有一些文字說明這個效果
 
-browser.quit()
+        # 她前往那個 URL - 她的待辦清單仍然在那裡。
+
+        # 她很滿意地上床睡覺
+
+        browser.quit()
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
