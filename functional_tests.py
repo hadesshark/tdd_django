2a7718eb9ad9f2747abed902e3f17d38aa2352f9
hadesshark <hades_shark@hotmail.com>
2017-02-23 11:56:25 +0800

    錯誤差不多，差別在判斷時處於什麼狀況。

--- functional_tests.py
+++ functional_tests.py
@@ -11,6 +11,11 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith 聽到一個很酷的新線上待辦事項 app。
         # 她去查看它的首頁
@@ -35,10 +40,7 @@ class NewVisitorTest(unittest.TestCase):
         # 當她按下 enter 時，網頁會更新，現在網頁列出
         # "1: 購買孔雀羽毛"，一個待辦事項清單項目
         inputbox.send_keys(Keys.ENTER)
-
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 此時仍然有一個文字方塊，讓她可以加入另一個項目。
         # 她輸入“使用孔雀羽毛來製作一隻蒼蠅”（Edith 非常有條理）
@@ -47,13 +49,12 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)
 
         # 網頁再次更新，現在她的清單有這兩個項目
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         import time
         time.sleep(10)
 
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('2: Use peacock feathers')
 
         time.sleep(10)
 
