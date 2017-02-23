040831435a456f9426d7be72ae4484e6dc799ac6
hadesshark <hades_shark@hotmail.com>
2017-02-23 11:45:50 +0800

    在 win 使用，在判斷 2. Use ... 的部分程式跑得太快所以 time.sleep ，預期錯誤。

--- functional_tests.py
+++ functional_tests.py
@@ -42,13 +42,29 @@ class NewVisitorTest(unittest.TestCase):
 
         # 此時仍然有一個文字方塊，讓她可以加入另一個項目。
         # 她輸入“使用孔雀羽毛來製作一隻蒼蠅”（Edith 非常有條理）
-        self.fail('Finish the test!')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
 
         # 網頁再次更新，現在她的清單有這兩個項目
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
 
+        import time
+        time.sleep(10)
+
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+
+        time.sleep(10)
+
+        self.assertIn(
+            '2: Use peacock feathers to make a fly',
+            [row.text for row in rows]
+        )
         # Edith 不知道網站能否記得她的清單
         # 接著她看到網站產生一個唯一的 URL 給她
         # 網頁有一些文字說明這個效果
+        self.fail('Finish the test!')
 
         # 她前往那個 URL - 她的待辦清單仍然在那裡。
 
