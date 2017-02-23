d2e98f5da8f292cfd32a105b10685ebcaf7b3785
hadesshark <hades_shark@hotmail.com>
2017-02-23 14:53:03 +0800

    重新導向要把之前的 tests.py 中的一部份刪掉。

--- lists/tests.py
+++ lists/tests.py
@@ -31,13 +31,6 @@ class HomePageTest(TestCase):
         self.assertEqual(response.status_code, 302)
         self.assertEqual(response['location'], '/')
 
-        self.assertIn('A new list item', response.content.decode())
-        expected_html = render_to_string(
-            'home.html',
-            {'new_item_text': 'A new list item'}
-        )
-        self.assertEqual(response.content.decode(), expected_html)
-
     def test_home_page_only_saves_items_when_necessary(self):
         request = HttpRequest()
         home_page(request)
