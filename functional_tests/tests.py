93af4bd659161a410065ea486f3b61e7aeaa2f7f
hadesshark <hades_shark@hotmail.com>
2017-02-27 19:59:03 +0800

    功能測試會錯很正常，沒有的 url 只讓單元測試通過而已

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -47,6 +47,8 @@ class NewVisitorTest(LiveServerTestCase):
         import time
         time.sleep(10)
 
+        edith_list_url = self.browser.current_url
+        self.assertRegex(edith_list_url, '/lists/.+')
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # 此時仍然有一個文字方塊，讓她可以加入另一個項目。
@@ -66,12 +68,38 @@ class NewVisitorTest(LiveServerTestCase):
 
         time.sleep(10)
 
-        # Edith 不知道網站能否記得她的清單
-        # 接著她看到網站產生一個唯一的 URL 給她
-        # 網頁有一些文字說明這個效果
-        self.fail('Finish the test!')
+        # 網頁再次更新，現在她的清單有這兩個項目
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        # 她前往那個 URL - 她的待辦清單仍然在那裡。
+        # 在新的使用者 Francis 來到網站
+
+        ## 我們使用一個新的瀏覽器工作階段來確保
+        ## Edith 的任何資訊都不會被 cookies 等機制送出
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Francis 造訪首頁。沒有任何 Edith 的清單的跡象
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+
+        # Francis 輸入一個新的項目，做出一個新的清單。
+        # 他比 Edith 無趣…
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+
+        # Francis 取得他自己的獨一無二 URL
+        francis_list_url = self.browser.current_url
+        self.assertRegex(francis_list_url, '/lists/.+')
+        self.assertNotEqual(francis_list_url, edith_list_url)
+
+        # 同樣的，沒有 Edith 的清單的任何跡象
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
 
         # 她很滿意地上床睡覺
 
